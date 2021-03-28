1ef569d817134990948aeac79304cf4462558669
Baptiste Petit <petit.baptiste21@gmail.com>
2021-03-28 23:44:35 +0900

    feat(dev_tools): add a devServer config to webpack to ease development

--- webpack.config.js
+++ webpack.config.js
@@ -34,4 +34,10 @@ module.exports = {
             template: 'src/index.html'
         }),
     ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        host: '0.0.0.0',
+        port: 9000,
+    },
 };
