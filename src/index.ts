8505caf87c2efd2189476c3d0bd00158a147a375
Baptiste Petit <petit.baptiste21@gmail.com>
2021-03-28 23:07:15 +0900

    feat(view/scene): add threejs cube example code as hello world

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,47 @@
 import '../style/main.css';
 
-console.log('Hello World!');
+import {
+    BoxGeometry,
+    Mesh,
+    MeshBasicMaterial,
+    PerspectiveCamera,
+    Scene,
+    WebGLRenderer,
+} from 'three';
+
+
+const container = document.getElementById('appContainer');
+const canvas = document.getElementById('scene') as HTMLCanvasElement;
+const renderer = new WebGLRenderer({canvas});
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const geometry = new BoxGeometry();
+const material = new MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new Mesh(geometry, material);
+scene.add(cube);
+
+function resizeCanvas() {
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+}
+
+const animate = function () {
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(animate);
+};
+
+function init() {
+    resizeCanvas();
+    animate();
+}
+
+
+window.addEventListener('resize', resizeCanvas);
+window.addEventListener('load', init);
