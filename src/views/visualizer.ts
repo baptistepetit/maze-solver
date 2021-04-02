35d0c1bd416c3a2d0cbf0a27e44644b2f040e399
Baptiste Petit <petit.baptiste21@gmail.com>
2021-04-02 23:18:53 +0900

    feat(visualizer): add camera controls

--- src/views/visualizer.ts
+++ src/views/visualizer.ts
@@ -7,12 +7,13 @@ import {
 } from 'three';
 
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 export class Visualizer {
     private camera: PerspectiveCamera;
     private container: HTMLDivElement;
     private renderer: WebGLRenderer;
-
+    private controls: OrbitControls;
     private scene: Scene;
 
     constructor(_canvas: HTMLCanvasElement, _container: HTMLDivElement) {
@@ -21,6 +22,8 @@ export class Visualizer {
         this.container = _container;
         this.renderer = new WebGLRenderer({ canvas: _canvas });
         this.scene = new Scene();
+        this.controls = new OrbitControls(this.camera, _canvas);
+        this.controls.update();
 
         const ambientLight = new AmbientLight(0xCCCCCC, 0.4);
         const directionalLight = new DirectionalLight();
