ca7f2455d134aa0cb654931fe77a7c9483850308
Baptiste Petit <petit.baptiste21@gmail.com>
2021-04-03 23:13:54 +0900

    feat(visualizer): fit camera frustum to object when loaded

--- src/views/visualizer.ts
+++ src/views/visualizer.ts
@@ -1,8 +1,11 @@
 import {
     AmbientLight,
+    Box3,
     DirectionalLight,
+    Object3D,
     PerspectiveCamera,
     Scene,
+    Vector3,
     WebGLRenderer,
 } from 'three';
 
@@ -36,10 +39,38 @@ export class Visualizer {
         requestAnimationFrame(this.animate.bind(this));
     }
 
+    private fitCameraToObject(object: Object3D, fitOffset = 1.5) {
+        const box = new Box3();
+        box.setFromObject(object);
+        const size = box.getSize(new Vector3());
+        const center = box.getCenter(new Vector3());
+
+        const maxSize = Math.max(size.x, size.y, size.z);
+        const fitHeightDistance = maxSize / (2 * Math.atan(Math.PI * this.camera.fov / 180));
+        const fitWidthDistance = fitHeightDistance / this.camera.aspect;
+        const distance = fitOffset * Math.max(fitHeightDistance, fitWidthDistance);
+
+        const direction = this.controls.target.clone()
+            .sub(this.camera.position)
+            .normalize()
+            .multiplyScalar(distance);
+
+        this.controls.maxDistance = distance * 10;
+        this.controls.target.copy(center);
+
+        this.camera.near = distance / 100;
+        this.camera.far = distance * 100;
+        this.camera.updateProjectionMatrix();
+
+        this.camera.position.copy(this.controls.target).sub(direction);
+        this.controls.update();
+    }
+
     loadMaze(obj: string): void {
         const objLoader = new OBJLoader();
         const mazeObj = objLoader.parse(obj);
         this.scene.add(mazeObj);
+        this.fitCameraToObject(mazeObj);
     }
 
     resize(): void {
