f866128a6908507824d159742f6ae833096511f8
Baptiste Petit <petit.baptiste21@gmail.com>
2021-04-24 22:34:21 +0900

    feat(models/maze): load path information from obj file

--- src/models/maze.ts
+++ src/models/maze.ts
@@ -1,3 +1,12 @@
+import { Point3D } from './geometry';
+
+interface ObjFace {
+    v1: number,
+    v2: number,
+    v3: number,
+    v4: number,
+}
+
 export class Maze {
     obj: string;
     startFace: string;
@@ -24,7 +33,40 @@ export class Maze {
         });
     }
 
+    // TODO: Separate Faces on load
+    // TODO: Support Tris
     loadFromText(text: string): void {
         this.obj = text;
+
+        const lines = text.split('\n');
+        let vertexCount = 0;
+        let faceCount = 0;
+        const ObjFaces: ObjFace[] = [];
+        const vertices: Point3D[] = [];
+
+        lines.forEach((line) => {
+            const cols = line.split(' ');
+            if (cols[0] === 'v') {
+                vertices[vertexCount] = {
+                    x: parseFloat(cols[1]),
+                    y: parseFloat(cols[2]),
+                    z: parseFloat(cols[3]),
+                };
+                vertexCount++;
+            }
+
+            if (cols[0] === 'f') {
+                // Assume all quads for now
+                ObjFaces[faceCount] = {
+                    v1: parseInt(cols[1]) - 1,
+                    v2: parseInt(cols[2]) - 1,
+                    v3: parseInt(cols[3]) - 1,
+                    v4: parseInt(cols[4]) - 1,
+                };
+                faceCount++;
+            }
+        });
+
+        console.log(pathTiles, vertices);
     }
 }
