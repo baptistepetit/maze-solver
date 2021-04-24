3fe8690dfff94f86a6ec32dae959149e4c833a5e
Baptiste Petit <petit.baptiste21@gmail.com>
2021-04-24 21:15:23 +0900

    feat(models/graph): add a graph class to the program

---
+++ src/models/graph.ts
@@ -0,0 +1,43 @@
+export type Node = number;
+
+export interface GraphEdge {
+    adjacent: Node,
+    weight: number,
+}
+
+export class Graph {
+    private nodes: Map<Node, GraphEdge[]>;
+
+    constructor() {
+        this.nodes = new Map<Node, GraphEdge[]>();
+    }
+
+    get order(): number {
+        return this.nodes.size;
+    }
+
+    addEdge(a: Node, b: Node, weight: number): void {
+        const aToB: GraphEdge = {
+            adjacent: b,
+            weight: weight,
+        };
+        if (this.nodes.has(a)) {
+            this.nodes.get(a).push(aToB);
+        } else {
+            this.nodes.set(a, [aToB]);
+        }
+        const bToA: GraphEdge = {
+            adjacent: a,
+            weight: weight,
+        };
+        if (this.nodes.has(b)) {
+            this.nodes.get(b).push(bToA);
+        } else {
+            this.nodes.set(b, [bToA]);
+        }
+    }
+
+    getEdges(node: Node): GraphEdge[] {
+        return this.nodes.get(node);
+    }
+}
