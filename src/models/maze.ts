bee8e30d52b7acd5e23fd8caa91914c73daac1ef
Baptiste Petit <petit.baptiste21@gmail.com>
2021-04-24 22:45:54 +0900

    feat(models/maze): add a function to build the maze graph representation

--- src/models/maze.ts
+++ src/models/maze.ts
@@ -1,4 +1,10 @@
-import { Point3D } from './geometry';
+import { Edge3D, Face3D, Point3D, DistanceBetweenFaces } from './geometry';
+import { Graph } from './graph';
+
+interface ObjEdge {
+    v1: number,
+    v2: number,
+}
 
 interface ObjFace {
     v1: number,
@@ -11,6 +17,7 @@ export class Maze {
     obj: string;
     startFace: string;
     endFace: string;
+    graph: Graph;
 
     constructor(file?: Blob) {
         if (file) {
@@ -18,6 +25,61 @@ export class Maze {
         }
         this.startFace = null;
         this.endFace = null;
+        this.graph = new Graph();
+    }
+
+    // Completely arbitrary function to ensure consistent
+    // equality after stringification
+    private orderEdge(edge: ObjEdge): ObjEdge {
+        if (edge.v1 < edge.v2) {
+            return edge;
+        } else {
+            return { v1: edge.v2, v2: edge.v1 };
+        }
+    }
+
+    private buildGraph(faces: ObjFace[], vertices: Point3D[]): void {
+        const edgeCommonFaces = new Map<string, number[]>();
+        faces.forEach((face, faceIndex) => {
+            const edges: ObjEdge[] = [];
+            // Assume all quads for now
+            edges.push({v1: face.v1, v2: face.v2});
+            edges.push({v1: face.v2, v2: face.v3});
+            edges.push({v1: face.v3, v2: face.v4});
+            edges.push({v1: face.v4, v2: face.v1});
+
+            edges.forEach((edge) => {
+                if (edgeCommonFaces.has(JSON.stringify(this.orderEdge(edge)))) {
+                    edgeCommonFaces.get(JSON.stringify(this.orderEdge(edge))).push(faceIndex);
+                } else {
+                    edgeCommonFaces.set(JSON.stringify(this.orderEdge(edge)), [faceIndex]);
+                }
+            });
+        });
+
+        edgeCommonFaces.forEach((value, key) => {
+            if (value.length == 2) {  // else is non-shared or impossible
+                const face1: Face3D = {
+                    p1: vertices[faces[value[0]].v1],
+                    p2: vertices[faces[value[0]].v2],
+                    p3: vertices[faces[value[0]].v3],
+                    p4: vertices[faces[value[0]].v4],
+                };
+                const face2: Face3D = {
+                    p1: vertices[faces[value[1]].v1],
+                    p2: vertices[faces[value[1]].v2],
+                    p3: vertices[faces[value[1]].v3],
+                    p4: vertices[faces[value[1]].v4],
+                };
+                const edgeIndices: ObjEdge = JSON.parse(key);
+                const commonEdge: Edge3D = {
+                    p1: vertices[edgeIndices.v1],
+                    p2: vertices[edgeIndices.v2],
+                };
+                const distance = DistanceBetweenFaces(face1, face2, commonEdge);
+                this.graph.addEdge(value[0], value[1], distance);
+            }
+        });
     }
 
     loadFromFile(file: Blob): Promise<void> {
@@ -67,6 +129,6 @@ export class Maze {
             }
         });
 
-        console.log(pathTiles, vertices);
+        this.buildGraph(ObjFaces, vertices);
     }
 }
